c7eed38f90031d632880a5be3cf71450cf7e21a9
Lumanyano gomomo <gomomolumanyano@gmail.com>
2020-03-26 16:41:59 +0200

    first commit

---
+++ isFromBellville.js
@@ -0,0 +1,5 @@
+describe("isFromBellville", function(){
+
+    it ("should give me all those from bellvile", function(){
+    })
+})
---
+++ test.greet.js
@@ -0,0 +1,16 @@
+describe('this test' , function(){
+    it('should show you how to use MochaJS' , function(){
+        assert.equal(2,2);
+
+        assert.deepEqual([2,2],[2,2]);
+    });
+ it("should greet Lindani with 'hello, Lindani'", function(){
+   assert.equal("hello, Lindani", greet("Lindani"))
+
+ })
+
+ it ("should greet Zola with 'hello, Zola'", function(){
+    assert.equal("hello, Zola", greet("Zola"))
+ })
+
+});
